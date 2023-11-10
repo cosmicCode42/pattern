e0feb4eb8a5dd9755d8e9eb993e4c55f9691949c
cosmicCode42 <cosmicanswer@gmail.com>
2023-11-10 13:37:01 +0000

    moved game notes to script.js, added wireframe, started script.js codde

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+// A simple pattern game. Currently planning 4 buttons pressed in order on the screen.
+// Idea: make patterns flash increasingly more quickly as time goes on [to a maximum speed].
+// Idea: increase pattern length as time goes on. Start at 3; end at 8?
+// Idea: reset pattern length with each increase in speed.
+
+const gameStuff = {
+    //Need to store the buttons, a turn counter, a pattern counter
+    buttons: ['button1', 'button2', 'button3', 'button4'],
+};
