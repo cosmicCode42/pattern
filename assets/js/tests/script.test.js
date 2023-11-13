7d1865b7513962132f8f4718f91f7aa2b63d5c06
cosmicCode42 <cosmicanswer@gmail.com>
2023-11-13 16:18:11 +0000

    added jest jsdom environment, added tests; jquery and jest don't play nice

--- assets/js/tests/script.test.js
+++ assets/js/tests/script.test.js
@@ -13,3 +13,30 @@ beforeAll(() => {
     document.write(fileContents);
     document.close();
 });
+
+describe("gameStuff object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in gameStuff).toBe(true);
+    });
+    test("buttons key exists", () => {
+        expect("buttons" in gameStuff).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in gameStuff).toBe(true);
+    });
+    test("playerInput key exists", () => {
+        expect("playerInput" in gameStuff).toBe(true);
+    });
+    test("patternCount key exists", () => {
+        expect("patternCount" in gameStuff).toBe(true);
+    });
+    test("turnCount key exists", () => {
+        expect("turnCount" in gameStuff).toBe(true);
+    });
+    test("lastButton key exists", () => {
+        expect("lastButton" in gameStuff).toBe(true);
+    });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in gameStuff).toBe(true);
+    });
+});
